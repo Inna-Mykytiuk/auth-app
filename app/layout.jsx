45667fd3791d8670ce5098f5740482d787c4a039
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-29 12:40:28 +0300

    add assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,8 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <main className="container">{children}</main>
+      <body className="bg-blue-400">
+        <main className=" bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
+          <div>Images</div>
+
+          <div className="right flex flex-col justify-evenly bg-gray-500">
+            <div className="text-center py-10">{children}</div>
+          </div>
+        </main>
       </body>
     </html>
   );
