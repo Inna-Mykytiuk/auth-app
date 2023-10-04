415d5b14d3b7bb9768b0ee874b3e6fd41adc24be
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-10-04 23:52:41 +0300

    clean garbage

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,6 @@
+import './globals.css';
 import { HomeSection } from '@/components/HomeSection';
-// import { Guest } from '@/components/Guest';
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
-import { LoginFormSection } from '@/components/LoginFormSection';
-import { authOptions } from './api/auth/[...nextauth]/route';
 
 export default function Home() {
-  // const session = await getServerSession(authOptions);
-
-  // if (session) redirect('/dashboard');
   return <HomeSection />;
 }
