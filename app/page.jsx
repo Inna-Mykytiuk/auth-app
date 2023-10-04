60230bded3a5f00b27fa3d768c26b607fd3a8ce8
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-10-04 16:41:27 +0300

    add guest page

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,11 @@ import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { LoginFormSection } from '@/components/LoginFormSection';
 import { authOptions } from './api/auth/[...nextauth]/route';
+import { Guest } from '@/components/Guest';
 
 export default async function Home() {
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  // if (session) redirect('/dashboard');
-  return <LoginFormSection />;
+  if (session) redirect('/dashboard');
+  return <Guest />;
 }
