9c978427b567b48285cc06a5a9cafe501df5e239
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-10-05 20:42:54 +0300

    add styles and profile page

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -2,6 +2,7 @@
 'use client';
 
 import Link from 'next/link';
+import styles from '../styles/Form.module.css';
 import { signOut } from 'next-auth/react';
 import { useSession } from 'next-auth/react';
 
@@ -9,30 +10,34 @@ function UserInfo() {
   const { data: session } = useSession();
 
   return (
-    <main className="container mx-auto text-center py-20">
-      <h3 className="text-4xl font-bold">Authorize User Homepage</h3>
-
-      <div>
-        Name: <span className="font-bold">{session?.user?.name}</span>
-      </div>
-      <div>
-        Email: <span className="font-bold">{session?.user?.email}</span>
+    <main className="container mx-auto text-center py-20 h-full justify-between flex flex-col">
+      <div className="flex">
+        <h3 className="text-4xl font-bold">Authorize User Homepage</h3>
       </div>
 
-      <button
-        className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-center"
-        // onClick={handleSignOut}
-        onClick={() => signOut()}
-      >
-        Sign Out
-      </button>
+      <div className="flex flex-col">
+        <div>
+          Name: <span className="font-bold">{session?.user?.name}</span>
+        </div>
+        <div>
+          Email: <span className="font-bold">{session?.user?.email}</span>
+        </div>
+      </div>
 
-      <div className="flex justify-center">
-        <Link href={'/profile'}>
-          <p className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50">
-            Profile Page
-          </p>
-        </Link>
+      <div className="flex flex-col gap-2">
+        <button
+          className={styles.button}
+          type="button"
+          onClick={() => signOut()}
+        >
+          Log out
+        </button>
+
+        <div className="flex justify-center">
+          <Link href={'/profile'} className="w-full">
+            <p className={styles.button}>Profile Page</p>
+          </Link>
+        </div>
       </div>
     </main>
   );
