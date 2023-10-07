af6dc464a177c7d95eff2e4a5202667b182d3295
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-10-07 14:16:42 +0300

    fix styles for homepage

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -12,7 +12,7 @@ function UserInfo() {
 
   return (
     <main className="container mx-auto  text-center py-20 h-full justify-between flex flex-col gap-6">
-      <div className="flex">
+      <div className="flex justify-center">
         <h3 className="text-4xl font-bold font-dancing">
           {session?.user?.name} Homepage
         </h3>
