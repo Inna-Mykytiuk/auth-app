235281b65884aacd8da9ba551b9358f07cfec12f
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-10-07 13:59:51 +0300

    add profile content

--- components/UserProfile.jsx
+++ components/UserProfile.jsx
@@ -1,14 +1,31 @@
 'use client';
 
 import Link from 'next/link';
+import styles from '../styles/Form.module.css';
 // import { useRouter } from 'next/navigation';
 // import { useSession } from 'next-auth/react';
 
 const UserProfile = () => {
   return (
-    <div>
-      <Link href={'/'}>Go back</Link>
-    </div>
+    <main className="container mx-auto text-center items-center py-20 h-full justify-between flex flex-col gap-6">
+      <div className="flex">
+        <h3 className="text-4xl font-bold font-dancing">Make</h3>
+      </div>
+      <div>
+        <p>
+          &quot;I am not the same, having seen the moon shine on the other side
+          of the world.&quot; - Mary Anne Radmacher
+        </p>
+      </div>
+
+      <Link href={'/'} className="w-full">
+        <p
+          className={`${styles.button} w-[300px] sm:w-full mx-auto my-auto md:m-0`}
+        >
+          Go back
+        </p>
+      </Link>
+    </main>
   );
 };
 
